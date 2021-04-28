5bfa51e886ce0e06ea65fd99173dcc65037165c2
hydrogen1999 <nguyencongthinh1999@gmail.com>
2021-04-28 21:35:23 +0700

    cmt

--- views/__init__.py
+++ views/__init__.py
@@ -8,10 +8,8 @@ def home():
         return render_template('index.html')
     else:
         return render_template('login.html')
+    return render_template("index.html",last_year=lastYear())
 
-@app.route('/', methods=["GET"])
-def viewLastYear():
-    return lastYear()
 
 # Register new user
 @app.route('/register', methods=["GET", "POST"])
