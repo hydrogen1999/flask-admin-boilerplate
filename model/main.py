e894802768a17a9375f9fc402a5c8ee01ed65678
hydrogen1999 <nguyencongthinh1999@gmail.com>
2021-05-27 21:14:39 +0700

    cmt

--- model/main.py
+++ model/main.py
@@ -13,6 +13,8 @@ from sklearn.svm import SVR
 from sklearn.ensemble import VotingRegressor
 import statsmodels.api as sm
 import json
+import matplotlib.pyplot as plt
+from pmdarima.arima import auto_arima
 
 df = pd.read_csv('data\order.csv')
 df['date'] = pd.to_datetime(df.date)
@@ -23,6 +25,9 @@ def totalYear():
     df2 = df[['total', 'year']]
     total_year = df2.groupby(['year'], as_index=False).sum(
     ).sort_values('year', ascending=True)
+    data=total_year[['year','total']]
+    data['year']=data['year'].astype(str)
+    data.to_json('total_year.json', orient='index')
     return total_year
 
 
@@ -73,7 +78,9 @@ def totalDate():
     df5 = df[['total', 'date']]
     total_date = df5.groupby(['date'], as_index=False).sum(
     ).sort_values('date', ascending=True)
-    return total_date
+    data1=total_date[['date','total']]
+    data1['date'] = pd.to_datetime(data1['date']).dt.date
+    return data1
 
 def lastDate():
     """[summary]
@@ -95,13 +102,18 @@ def saleMonth():
     df7 = df[['quantity', 'month_year']]
     sale_month = df7.groupby(['month_year'], as_index=False).sum(
     ).sort_values('month_year', ascending=True)
+    data1=sale_month[['month_year','quantity']]
+    data1['month_year']=data1['month_year'].astype(str)
+    data1.to_json('sale_month.json', orient='index')
     return sale_month
 
 def saleDate():
     df8 = df[['quantity', 'date']]
     sale_date = df8.groupby(['date'], as_index=False).sum(
     ).sort_values('date', ascending=True)
-    return sale_date
+    data1=sale_date[['date','quantity']]
+    data1['date'] = pd.to_datetime(data1['date']).dt.date
+    return data1
 
 def lastYearSale():
     lastYearSale = saleYear().tail(1)
@@ -124,3 +136,79 @@ def percentageMethod():
     percent.to_json('percent.json', orient='split')
     return percent
 
+def stationary_trend(data):
+    result = sm.tsa.adfuller(data.dropna(), regression='c')
+    # print('ADF Statistic: %f' % result[0])
+    # print('p-value: %f' % result[1])
+    # print('Critical Values:')
+    # for key, value in result[4].items():
+    #     print('\t%s: %.3f' % (key, value))
+    return result
+
+
+def stationary(data):
+    result = sm.tsa.adfuller(data.dropna(), regression='ct')
+    # print('ADF Statistic: %f' % result[0])
+    # print('p-value: %f' % result[1])
+    # print('Critical Values:')
+    # for key, value in result[4].items():
+    #     print('\t%s: %.3f' % (key, value))
+    return result
+
+
+def fit_model_stationary(data):
+    model = auto_arima(data, start_p=0, start_q=0,
+                       max_p=5, max_q=5, m=12,
+                       start_P=0, seasonal=True,
+                       d=0, D=1, trace=True,
+                       error_action='ignore',
+                       suppress_warnings=True,
+                       stepwise=True)
+
+    print(model.aic())
+    return model
+
+def fit_model_non_stationary(data):
+    model = auto_arima(data, start_p=0, start_q=0,
+                       max_p=5, max_q=5, m=12,
+                       start_P=0, seasonal=True,
+                       d=1, D=1, trace=True,
+                       error_action='ignore',
+                       suppress_warnings=True,
+                       stepwise=True)
+
+    print(model.aic())
+    return model
+
+def fit_model_fast(data):
+    model = auto_arima(data, start_p=5, start_q=0,
+                           max_p=5, max_q=0, m=12,start_Q=0, max_Q=0,
+                           start_P=2,max_P=2, seasonal=True,
+                           d=1, D=1, trace=True,
+                           error_action='ignore',  
+                           suppress_warnings=True, 
+                           stepwise=True)
+
+    print(model.aic())
+    return model
+
+def fit_model_fast_stationary(data):
+    model = auto_arima(data, start_p=5, start_q=0,
+                           max_p=5, m=12,start_Q=0, max_Q=0,
+                           start_P=2, seasonal=True,
+                           d=0, D=1, trace=True,
+                           error_action='ignore',  
+                           suppress_warnings=True, 
+                           stepwise=True)
+
+    print(model.aic())
+    return model
+
+# def show_result(model):
+#     return model.summary()
+
+# def check_model(model):
+#     model_sarima.plot_diagnostics(figsize=(15, 12))
+#     url='/static/images/plot.png'
+#     plt.savefig(url)
+#     return url
--- views/__init__.py
+++ views/__init__.py
@@ -6,6 +6,13 @@ import json
 import pandas as pd
 import numpy as np
 
+class DataStore():
+    model=None
+    model_month=None
+    sale_model=None
+
+data = DataStore()
+
 @app.route('/', methods=["GET"])
 def home():
     percent=percentageMethod()
@@ -75,20 +82,441 @@ def errorpage():
 def blank():
     return render_template('blank.html')
 
-#Buttons Page
-@app.route('/buttons', methods=["GET"])
-def buttons():
-    return render_template("buttons.html")
+@app.route('/totalyear', methods=["GET"])
+def total_year():
+    total_year=totalYear()
+    file1=pd.read_json('total_year.json',orient='index')
+    year_index=np.array(file1['year'])
+    year_data=np.array(file1['total']) 
+    return render_template("total_year.html",year_index=year_index, year_data=year_data)
 
-#Cards Page
-@app.route('/cards', methods=["GET"])
-def cards():
-    return render_template('cards.html')
+@app.route('/totalmonth', methods=["GET"])
+def total_month():
+    total_month=totalMonth()
+    file1=pd.read_json('total_month.json',orient='index')
+    month_index=np.array(file1['month_year'])
+    month_data=np.array(file1['total'])
+    num=6
+    # Fit model
+    model=fit_model()
+    data.model_month=model
+    predict_rs, fitted_data=predict(model,6)
+    pred_index=np.array(predict_rs['month_year'])
+    pred_data=np.array(predict_rs['total'])
+    #Test model
+    test_rs= test(pred_data[0], fitted_data)
+    return render_template("total_month.html",month_index=month_index, month_data=month_data, stationary=check_stationary(), model=model, pred_index=pred_index, pred_data=pred_data, test_rs=test_rs, num=num)
+def check_stationary():
+    total_month=totalMonth()
+    data1=total_month[['month_year','total']]
+    data1.set_index('month_year', inplace=True)
+    result=stationary(data1)
+    return result
+def fit_model():
+    total_month=totalMonth()
+    data1=total_month[['month_year','total']]
+    data1.set_index('month_year', inplace=True)
+    data=data1['total']
+    stationary=check_stationary()
+    p=stationary[1]
+    if (p<0.05):
+        result1 = fit_model_stationary(data)
+    else:
+        result1 = fit_model_non_stationary(data)
+    return result1
+def predict(model,num_predict):
+    if num_predict==0:
+        num_predict=6
+    fitted_month, confint_month = model.predict(n_periods=num_predict, return_conf_int=True)
+    df2=df[['total', 'date']]
+    total_day = df2.groupby(['date'], as_index=False).sum().sort_values('date', ascending=True)
+    data=total_day[['date','total']]
+    data.set_index('date', inplace=True)
+    date = pd.date_range(data.index[-1], periods=num_predict, freq='MS')
+    fitted_seri_month = pd.Series(fitted_month, index=date)
+    dff=pd.DataFrame(fitted_seri_month)
+    dff=dff.reset_index()
+    dff.columns=['date','total']
+    dff['month_year'] = pd.to_datetime(dff['date']).dt.to_period('M')
+    pred=dff[['month_year','total']]
+    return pred, fitted_month
+def test(y, yhat):
+    e = y-yhat
+    mse=np.mean(e**2)
+    rmse=np.sqrt(mse)
+    mae=np.mean(np.abs(e))
+    mape=np.mean(abs(e/y))
 
-#Charts Page
-@app.route('/charts', methods=["GET"])
-def charts():
-    return render_template("charts.html")
+    # print('Sai số bình phương trung bình MSE: {}'.format(mse))
+    # print('Root Mean Square Error: {}'.format(rmse))
+    # print('Mean Absolute Error: {}'.format(mae))
+    # print('Mean Absolute Percentage Error: {}'.format(mape))
+    return mse, rmse, mae, mape
+@app.route('/totalmonth', methods=["POST"])
+def total_month_num():
+    total_month=totalMonth()
+    file1=pd.read_json('total_month.json',orient='index')
+    month_index=np.array(file1['month_year'])
+    month_data=np.array(file1['total'])
+    #Get data
+    if request.method == "POST":
+        num = int(request.form.get("num_month"))
+    predict_rs, fitted_data=predict(data.model_month,num)
+    pred_index=np.array(predict_rs['month_year'])
+    pred_data=np.array(predict_rs['total'])
+    #Test model
+    test_rs= test(pred_data[0], fitted_data)
+    return render_template("total_month.html",month_index=month_index, month_data=month_data, stationary=check_stationary(), model=data.model_month, pred_index=pred_index, pred_data=pred_data, test_rs=test_rs, num=num)
+def check_stationary():
+    total_month=totalMonth()
+    data1=total_month[['month_year','total']]
+    data1.set_index('month_year', inplace=True)
+    result=stationary(data1)
+    return result
+def predict(model,num_predict):
+    if num_predict==0:
+        num_predict=6
+    fitted_month, confint_month = model.predict(n_periods=num_predict, return_conf_int=True)
+    df2=df[['total', 'date']]
+    total_day = df2.groupby(['date'], as_index=False).sum().sort_values('date', ascending=True)
+    data2=total_day[['date','total']]
+    data2.set_index('date', inplace=True)
+    date = pd.date_range(data2.index[-1], periods=num_predict, freq='MS')
+    fitted_seri_month = pd.Series(fitted_month, index=date)
+    dff=pd.DataFrame(fitted_seri_month)
+    dff=dff.reset_index()
+    dff.columns=['date','total']
+    dff['month_year'] = pd.to_datetime(dff['date']).dt.to_period('M')
+    pred=dff[['month_year','total']]
+    return pred, fitted_month
+def test(y, yhat):
+    e = y-yhat
+    mse=np.mean(e**2)
+    rmse=np.sqrt(mse)
+    mae=np.mean(np.abs(e))
+    mape=np.mean(abs(e/y))
+
+    # print('Sai số bình phương trung bình MSE: {}'.format(mse))
+    # print('Root Mean Square Error: {}'.format(rmse))
+    # print('Mean Absolute Error: {}'.format(mae))
+    # print('Mean Absolute Percentage Error: {}'.format(mape))
+    return mse, rmse, mae, mape
+@app.route('/totaldate', methods=["GET"])
+def total_date():
+    total_date=totalDate()
+    date_index=np.array(total_date['date'])
+    date_data=np.array(total_date['total'])
+    num=30
+    # Fit model
+    model_date=fit_model_date()
+    data.model=model_date
+    predict_rs_date, fitted_data_date=predict_date(model_date,30)
+    pred_index_date=np.array(predict_rs_date['date'])
+    pred_data_date=np.array(predict_rs_date['total'])
+    #Test model
+    test_rs= test_date(pred_data_date[0], fitted_data_date)
+    return render_template("total_date.html",date_index=date_index, date_data=date_data, stationary=check_stationary_date(), model_date=model_date, pred_index=pred_index_date, pred_data=pred_data_date, test_rs=test_rs, num=num)
+def check_stationary_date():
+    total_date=totalDate()
+    data1=total_date[['date','total']]
+    data1.set_index('date', inplace=True)
+    result=stationary_trend(data1)
+    return result
+def fit_model_date():
+    total_date=totalDate()
+    data1=total_date[['date','total']]
+    data1.set_index('date', inplace=True)
+    data=data1['total']
+    result1 = fit_model_fast(data)
+    return result1
+def predict_date(model_date, num_predict):
+    if num_predict==0:
+        num_predict=30
+    fitted_date, confint_date = model_date.predict(n_periods=num_predict, return_conf_int=True)
+    df2=df[['total', 'date']]
+    total_day = df2.groupby(['date'], as_index=False).sum().sort_values('date', ascending=True)
+    data=total_day[['date','total']]
+    data.set_index('date', inplace=True)
+    date = pd.date_range(data.index[-1], periods=num_predict)
+    fitted_seri_date = pd.Series(fitted_date, index=date)
+    dff=pd.DataFrame(fitted_seri_date)
+    dff=dff.reset_index()
+    dff.columns=['date','total']
+    dff['date'] = pd.to_datetime(dff['date']).dt.to_period('D')
+    pred=dff[['date','total']]
+    return pred, fitted_date
+def test_date(y, yhat):
+    e = y-yhat
+    mse=np.mean(e**2)
+    rmse=np.sqrt(mse)
+    mae=np.mean(np.abs(e))
+    mape=np.mean(abs(e/y))
+
+    # print('Sai số bình phương trung bình MSE: {}'.format(mse))
+    # print('Root Mean Square Error: {}'.format(rmse))
+    # print('Mean Absolute Error: {}'.format(mae))
+    # print('Mean Absolute Percentage Error: {}'.format(mape))
+    return mse, rmse, mae, mape
+
+@app.route('/totaldate', methods=["POST"])
+def total_date_num():
+    total_date=totalDate()
+    date_index=np.array(total_date['date'])
+    date_data=np.array(total_date['total'])
+    #Get data
+    if request.method == "POST":
+        num = int(request.form.get("num_date"))
+    predict_rs_date, fitted_data_date=predict_date(data.model,num)
+    pred_index_date=np.array(predict_rs_date['date'])
+    pred_data_date=np.array(predict_rs_date['total'])
+    test_rs= test_date(pred_data_date[0], fitted_data_date)
+    return render_template("total_date.html",date_index=date_index, date_data=date_data, stationary=check_stationary_date(), model_date=data.model, pred_index=pred_index_date, pred_data=pred_data_date, test_rs=test_rs, num=num)
+def check_stationary_date():
+    total_date=totalDate()
+    data1=total_date[['date','total']]
+    data1.set_index('date', inplace=True)
+    result=stationary_trend(data1)
+    return result
+def predict_date(model_date, num_predict):
+    if num_predict==0:
+        num_predict=6
+    fitted_date, confint_date = model_date.predict(n_periods=num_predict, return_conf_int=True)
+    df2=df[['total', 'date']]
+    total_day = df2.groupby(['date'], as_index=False).sum().sort_values('date', ascending=True)
+    data2=total_day[['date','total']]
+    data2.set_index('date', inplace=True)
+    date = pd.date_range(data2.index[-1], periods=num_predict)
+    fitted_seri_date = pd.Series(fitted_date, index=date)
+    dff=pd.DataFrame(fitted_seri_date)
+    dff=dff.reset_index()
+    dff.columns=['date','total']
+    dff['date'] = pd.to_datetime(dff['date']).dt.to_period('D')
+    pred=dff[['date','total']]
+    return pred, fitted_date
+def test_date(y, yhat):
+    e = y-yhat
+    mse=np.mean(e**2)
+    rmse=np.sqrt(mse)
+    mae=np.mean(np.abs(e))
+    mape=np.mean(abs(e/y))
+    return mse, rmse, mae, mape
+@app.route('/revenueyear', methods=["GET"])
+def revenue_year():
+    sale_year=saleYear()
+    year_index=np.array(sale_year['year'])
+    year_data=np.array(sale_year['quantity']) 
+    return render_template("revenue_year.html",year_index=year_index, year_data=year_data)
+
+@app.route('/revenuemonth', methods=["GET"])
+def revenue_month():
+    total_month=saleMonth()
+    file1=pd.read_json('sale_month.json',orient='index')
+    month_index=np.array(file1['month_year'])
+    month_data=np.array(file1['quantity'])
+    num_sale=6
+    # Fit model
+    model=fit_model()
+    data.model_month=model
+    predict_rs, fitted_data=predict(model,6)
+    pred_index=np.array(predict_rs['month_year'])
+    pred_data=np.array(predict_rs['quantity'])
+    #Test model
+    test_rs= test(pred_data[0], fitted_data)
+    return render_template("revenue_month.html",month_index=month_index, month_data=month_data, stationary=check_stationary(), model=model, pred_index=pred_index, pred_data=pred_data, test_rs=test_rs, num_sale=num_sale)
+def check_stationary():
+    total_month=saleMonth()
+    data1=total_month[['month_year','quantity']]
+    data1.set_index('month_year', inplace=True)
+    result=stationary(data1)
+    return result
+def fit_model():
+    total_month=saleMonth()
+    data1=total_month[['month_year','quantity']]
+    data1.set_index('month_year', inplace=True)
+    data=data1['quantity']
+    stationary=check_stationary()
+    p=stationary[1]
+    if (p<0.05):
+        result1 = fit_model_stationary(data)
+    else:
+        result1 = fit_model_non_stationary(data)
+    return result1
+def predict(model,num_predict):
+    if num_predict==0:
+        num_predict=6
+    fitted_month, confint_month = model.predict(n_periods=num_predict, return_conf_int=True)
+    df2=df[['quantity', 'date']]
+    total_day = df2.groupby(['date'], as_index=False).sum().sort_values('date', ascending=True)
+    data=total_day[['date','quantity']]
+    data.set_index('date', inplace=True)
+    date = pd.date_range(data.index[-1], periods=num_predict, freq='MS')
+    fitted_seri_month = pd.Series(fitted_month, index=date)
+    dff=pd.DataFrame(fitted_seri_month)
+    dff=dff.reset_index()
+    dff.columns=['date','quantity']
+    dff['month_year'] = pd.to_datetime(dff['date']).dt.to_period('M')
+    pred=dff[['month_year','quantity']]
+    return pred, fitted_month
+def test(y, yhat):
+    e = y-yhat
+    mse=np.mean(e**2)
+    rmse=np.sqrt(mse)
+    mae=np.mean(np.abs(e))
+    mape=np.mean(abs(e/y))
+
+    # print('Sai số bình phương trung bình MSE: {}'.format(mse))
+    # print('Root Mean Square Error: {}'.format(rmse))
+    # print('Mean Absolute Error: {}'.format(mae))
+    # print('Mean Absolute Percentage Error: {}'.format(mape))
+    return mse, rmse, mae, mape
+@app.route('/revenuemonth', methods=["POST"])
+def revenue_month_num():
+    total_month=saleMonth()
+    file1=pd.read_json('sale_month.json',orient='index')
+    month_index=np.array(file1['month_year'])
+    month_data=np.array(file1['quantity'])
+    #Get data
+    if request.method == "POST":
+        num_sale= int(request.form.get("sale_month"))
+    predict_rs, fitted_data=predict(data.model_month,num_sale)
+    pred_index=np.array(predict_rs['month_year'])
+    pred_data=np.array(predict_rs['quantity'])
+    #Test model
+    test_rs= test(pred_data[0], fitted_data)
+    return render_template("revenue_month.html",month_index=month_index, month_data=month_data, stationary=check_stationary(), model=data.model_month, pred_index=pred_index, pred_data=pred_data, test_rs=test_rs, num_sale=num_sale)
+def check_stationary():
+    total_month=saleMonth()
+    data1=total_month[['month_year','quantity']]
+    data1.set_index('month_year', inplace=True)
+    result=stationary(data1)
+    return result
+def predict(model,num_predict):
+    if num_predict==0:
+        num_predict=6
+    fitted_month, confint_month = model.predict(n_periods=num_predict, return_conf_int=True)
+    df2=df[['quantity', 'date']]
+    total_day = df2.groupby(['date'], as_index=False).sum().sort_values('date', ascending=True)
+    data2=total_day[['date','quantity']]
+    data2.set_index('date', inplace=True)
+    date = pd.date_range(data2.index[-1], periods=num_predict, freq='MS')
+    fitted_seri_month = pd.Series(fitted_month, index=date)
+    dff=pd.DataFrame(fitted_seri_month)
+    dff=dff.reset_index()
+    dff.columns=['date','quantity']
+    dff['month_year'] = pd.to_datetime(dff['date']).dt.to_period('M')
+    pred=dff[['month_year','quantity']]
+    return pred, fitted_month
+def test(y, yhat):
+    e = y-yhat
+    mse=np.mean(e**2)
+    rmse=np.sqrt(mse)
+    mae=np.mean(np.abs(e))
+    mape=np.mean(abs(e/y))
+
+    # print('Sai số bình phương trung bình MSE: {}'.format(mse))
+    # print('Root Mean Square Error: {}'.format(rmse))
+    # print('Mean Absolute Error: {}'.format(mae))
+    # print('Mean Absolute Percentage Error: {}'.format(mape))
+    return mse, rmse, mae, mape
+@app.route('/revenuedate', methods=["GET"])
+def revenue_date():
+    total_date=saleDate()
+    date_index=np.array(total_date['date'])
+    date_data=np.array(total_date['quantity'])
+    num=30
+    # Fit model
+    model_date=fit_model_date()
+    data.sale_model=model_date
+    predict_rs_date, fitted_data_date=predict_date(model_date,30)
+    pred_index_date=np.array(predict_rs_date['date'])
+    pred_data_date=np.array(predict_rs_date['quantity'])
+    #Test model
+    test_rs= test_date(pred_data_date[0], fitted_data_date)
+    return render_template("revenue_date.html",date_index=date_index, date_data=date_data, stationary=check_stationary_date(), model_date=model_date, pred_index=pred_index_date, pred_data=pred_data_date, test_rs=test_rs, num=num)
+def check_stationary_date():
+    total_date=saleDate()
+    data1=total_date[['date','quantity']]
+    data1.set_index('date', inplace=True)
+    result=stationary_trend(data1)
+    return result
+def fit_model_date():
+    total_date=saleDate()
+    data1=total_date[['date','quantity']]
+    data1.set_index('date', inplace=True)
+    data=data1['quantity']
+    result1 = fit_model_fast(data)
+    return result1
+def predict_date(model_date, num_predict):
+    if num_predict==0:
+        num_predict=30
+    fitted_date, confint_date = model_date.predict(n_periods=num_predict, return_conf_int=True)
+    df2=df[['quantity', 'date']]
+    total_day = df2.groupby(['date'], as_index=False).sum().sort_values('date', ascending=True)
+    data=total_day[['date','quantity']]
+    data.set_index('date', inplace=True)
+    date = pd.date_range(data.index[-1], periods=num_predict)
+    fitted_seri_date = pd.Series(fitted_date, index=date)
+    dff=pd.DataFrame(fitted_seri_date)
+    dff=dff.reset_index()
+    dff.columns=['date','quantity']
+    dff['date'] = pd.to_datetime(dff['date']).dt.to_period('D')
+    pred=dff[['date','quantity']]
+    return pred, fitted_date
+def test_date(y, yhat):
+    e = y-yhat
+    mse=np.mean(e**2)
+    rmse=np.sqrt(mse)
+    mae=np.mean(np.abs(e))
+    mape=np.mean(abs(e/y))
+
+    # print('Sai số bình phương trung bình MSE: {}'.format(mse))
+    # print('Root Mean Square Error: {}'.format(rmse))
+    # print('Mean Absolute Error: {}'.format(mae))
+    # print('Mean Absolute Percentage Error: {}'.format(mape))
+    return mse, rmse, mae, mape
+
+@app.route('/revenuedate', methods=["POST"])
+def revenue_date_num():
+    total_date=saleDate()
+    date_index=np.array(total_date['date'])
+    date_data=np.array(total_date['quantity'])
+    #Get data
+    if request.method == "POST":
+        num = int(request.form.get("sale_date"))
+    predict_rs_date, fitted_data_date=predict_date(data.sale_model,num)
+    pred_index_date=np.array(predict_rs_date['date'])
+    pred_data_date=np.array(predict_rs_date['quantity'])
+    test_rs= test_date(pred_data_date[0], fitted_data_date)
+    return render_template("revenue_date.html",date_index=date_index, date_data=date_data, stationary=check_stationary_date(), model_date=data.sale_model, pred_index=pred_index_date, pred_data=pred_data_date, test_rs=test_rs, num=num)
+def check_stationary_date():
+    total_date=saleDate()
+    data1=total_date[['date','quantity']]
+    data1.set_index('date', inplace=True)
+    result=stationary_trend(data1)
+    return result
+def predict_date(model_date, num_predict):
+    if num_predict==0:
+        num_predict=6
+    fitted_date, confint_date = model_date.predict(n_periods=num_predict, return_conf_int=True)
+    df2=df[['quantity', 'date']]
+    total_day = df2.groupby(['date'], as_index=False).sum().sort_values('date', ascending=True)
+    data2=total_day[['date','quantity']]
+    data2.set_index('date', inplace=True)
+    date = pd.date_range(data2.index[-1], periods=num_predict)
+    fitted_seri_date = pd.Series(fitted_date, index=date)
+    dff=pd.DataFrame(fitted_seri_date)
+    dff=dff.reset_index()
+    dff.columns=['date','quantity']
+    dff['date'] = pd.to_datetime(dff['date']).dt.to_period('D')
+    pred=dff[['date','quantity']]
+    return pred, fitted_date
+def test_date(y, yhat):
+    e = y-yhat
+    mse=np.mean(e**2)
+    rmse=np.sqrt(mse)
+    mae=np.mean(np.abs(e))
+    mape=np.mean(abs(e/y))
+    return mse, rmse, mae, mape
 
 #Tables Page
 @app.route('/tables', methods=["GET"])
